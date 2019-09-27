73f26863f16b470819107312a9616677aaebd6d8
Walter Satriano <walter.satri@gmail.com>
2019-09-27 17:30:36 +0200

    definizione route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+  Generare nuovo progetto laravel con 2 endpoint:
+  - /cube/number
+  - /cube/letter
+
+  Nel primo troveremo 10 quadrati con le 10 cifre (0~9) su sfondo arancione
+  Nel secondo avremo le 25 quadrati con le lettere dell'alfabeto (A~Z) su sfondo rosso
+
+  Header e footer di enbrambe le pagine devono riportare il contenuto dei
+  quadrati (lettere o numeri) con lo stesso colore di sfondo dei quadrati,
+  fermo restando che devono essere condivisi tra le 2 pagine
+  attraverlo la tecnica yield/extends
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/cube/number", "MyController@getNumbersCube");
+Route::get("/cube/letter", "MyController@getLettersCube");
