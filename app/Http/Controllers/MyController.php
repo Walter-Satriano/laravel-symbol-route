19ca6d66094e6309e2bc6ea956020d53bcd8e37e
Walter Satriano <walter.satri@gmail.com>
2019-09-27 17:31:17 +0200

    creato controller ed inserite funzioni per pagina numeri

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -4,7 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MyController extends Controller
-{
-    //
+class MyController extends Controller {
+
+  private function getNumbers() {
+
+    $arr = [];
+    for ($i = 0; $i < 10 ; $i++) {
+
+      $arr[] = $i;
+    }
+    return $arr;
+  }
+
+  private function getLetters() {
+    // code...
+  }
+
+  public function getNumbersCube() {
+
+    $arrNum = $this -> getNumbers();
+
+    return view("number", compact("arrNum"));
+  }
+
+  public function getLettersCube() {
+    // code...
+  }
 }
