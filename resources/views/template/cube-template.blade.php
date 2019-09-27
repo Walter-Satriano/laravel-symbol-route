b35b8476a9a9b67f13b6d994270dbc26ef7c1b37
Walter Satriano <walter.satri@gmail.com>
2019-09-27 19:02:32 +0200

    creazione template

--- resources/views/template/cube-template.blade.php
+++ resources/views/template/cube-template.blade.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-    </head>
-    <body>
+  </head>
 
-      @include("elem.header")
+  <body>
+    
+    @include("elem.header")
 
-      <div class="box_number">
-        @yield("numbers")
-      </div>
+    <div class="box_number">
+      @yield("numbers")
+    </div>
 
-      @include("elem.footer")
+    <div class="box_lett">
+      @yield("letters")
+    </div>
 
-    </body>
+    @include("elem.footer")
+
+  </body>
 </html>
